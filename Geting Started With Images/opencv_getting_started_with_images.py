83b9001f1c83f12455e31cf5740feb522e83e1b4
pasanjg <44201430+pasanjg@users.noreply.github.com>
2019-11-14 05:28:46 +0530

    update filenames

--- Geting Started With Images/opencv_getting_started_with_images.py
+++ Geting Started With Images/opencv_getting_started_with_images.py
@@ -10,5 +10,5 @@ key = cv2.waitKey(0) & 0xFF  # add a delay to window. 0 - window will not disapp
 if key == 27:  # press 'Esc' to exit without saving
     cv2.destroyAllWindows()  # close all windows
 elif key == ord('s'):  # press 's' to save and exit
-    cv2.imwrite('lena-copy.png', img)  # write image to new file
+    cv2.imwrite('../images/lena-copy.png', img)  # write image to new file
     cv2.destroyAllWindows()
